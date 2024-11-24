509267e5f3f3a658aa2e53f9f69e72eb79e66748
Leader2580 <leadergunman@gmail.com>
2024-11-24 15:31:53 +0530

    Create route.py

---
+++ plugins/route.py
@@ -0,0 +1,14 @@
+#(©)PXL_Community
+#rymme
+
+
+
+
+
+from aiohttp import web
+
+routes = web.RouteTableDef()
+
+@routes.get("/", allow_head=True)
+async def root_route_handler(request):
+    return web.json_response("Codeflix_Bots (Telegram)")
